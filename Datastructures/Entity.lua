79b543ea4903c01cbdc484ad0992be96e9f98a16
Raild3x <47638612+Raild3x@users.noreply.github.com>
2020-02-17 22:51:15 -0500

    Implemented basic collision resolution

--- Datastructures/Entity.lua
+++ Datastructures/Entity.lua
@@ -1,10 +1,12 @@
+local Vector2 = require("Datastructures.Vector2");
+
 local Entity = {};
 Entity.__index = Entity;
 
 function Entity.new(x,y,width,height)
     local self = setmetatable({
-        x = x,
-        y = y,
+        pos = Vector2.New(x,y),
+        velocity = Vector2.New(0,0),
         width = width,
         height = height,
         rotation = 0,
@@ -16,22 +18,22 @@ end
 
 function Entity:Draw()
     if self.intersecting then
-        love.graphics.setColor(0,255,255);
+        love.graphics.setColor(0,200,255);
     else
-        love.graphics.setColor(0,255,0);
+        love.graphics.setColor(50,255,0);
     end
-    love.graphics.circle("line",self.x,self.y,self.width/2);
+    love.graphics.circle("line",self.pos.x,self.pos.y,self.width/2);
     self.intersecting = false
 end
 
 function Entity:Move(dx,dy)
-    self.x = self.x+dx;
-    self.y = self.y+dy;
+    self.velocity.x = dx;
+    self.velocity.y = dy;
 end
 
 function Entity:IntersectsRadius(entity)
     if self == entity then return false end
-    if math.sqrt((entity.x-self.x)^2+(entity.y-self.y)^2) <= self.width/2+entity.width/2 then
+    if Vector2.Magnitude(entity.pos-self.pos) <= self.width/2+entity.width/2 then
         self.intersecting = true;
         entity.intersecting = true;
         return true
@@ -41,7 +43,7 @@ end
 function Entity:AABB()
     local w = self.width/2
     local h = self.height/2
-    return self.x-w,self.x+w,self.y-h,self.y+h;
+    return self.pos.x-w,self.pos.x+w,self.pos.y-h,self.pos.y+h;
 end
 
 return Entity;
--- main.lua
+++ main.lua
@@ -8,17 +8,30 @@ local nodesPerLine = 10
 love.window.setMode(windowWidth, windowWidth)
 local grid = LTDG.new(nodesPerLine,nodesPerLine,windowWidth/nodesPerLine,windowWidth/nodesPerLine)
 
-local plr = Entity.new(88, 94, 20, 20)
+local size = 45
+local plr = Entity.new(88, 94, size, size)
 grid:Insert(plr);
 local boids = {plr}
-for i = 2, 5000 do
-    local d = math.floor(math.random()*100+1)
-    local b = Entity.new(math.floor(math.random()*800), math.floor(math.random()*800), d, d);
+for i = 2, 2000 do
+    local d = math.floor(math.random()*20+1); -- diameter
+    local b = Entity.new(math.floor(math.random()*(windowWidth-100)+50), math.floor(math.random()*(windowWidth-100)+50), d, d);
     print(b.x,b.y,d)
     table.insert(boids, b);
     grid:Insert(b);
 end
 
+
+local function resolveCollision(e, intersections)
+    for i = 1, #intersections do
+        local e2 = intersections[i];
+        local dir = (e.pos-e2.pos):Normalize()/5
+        --[[local midPoint = e2.pos+(dir/2);
+        e2.pos = midPoint+((e2.pos-midPoint):Normalize()*(e2.width/2));
+        e.pos = midPoint+((e.pos-midPoint):Normalize()*(e.width/2));]]
+        e2.pos:Add(dir*-1);
+        e.pos:Add(dir);
+    end
+end
 
 function love.mousepressed(x, y, button, istouch)
     if button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
@@ -35,29 +48,35 @@ function love.mousepressed(x, y, button, istouch)
 
 
 function love.draw()
-    
+    local x,y = 0,0
     if love.keyboard.isDown('w') then
-        plr:Move(0,-1);
+        y = -1
     end
     if love.keyboard.isDown('a') then
-        plr:Move(-1,0);
+        x = -1
     end
     if love.keyboard.isDown('s') then
-        plr:Move(0,1);
+        y = 1
     end
     if love.keyboard.isDown('d') then
-        plr:Move(1,0);
+        x = 1
     end
+    plr:Move(x,y);
+    plr.pos:Add(plr.velocity);
     grid:Insert(plr);
+    
+    local intersections = grid:SearchBox(plr);
+    resolveCollision(plr,intersections);
     plr:Draw();
 
-    local intersections = grid:SearchBox(plr);
-
     for i = 2, #boids do
-        boids[i]:Move(math.random()*2-1,math.random()*2-1)
-        grid:Insert(boids[i]);
-        grid:SearchBox(boids[i]);
-        boids[i]:Draw();
+        local b = boids[i];
+        --b:Move(math.random()*2-1,math.random()*2-1)
+        --b.pos:Add(b.velocity);
+        grid:Insert(b);
+        local intersections = grid:SearchBox(b);
+        resolveCollision(b,intersections);
+        b:Draw();
     end
     grid:Draw();
 end
